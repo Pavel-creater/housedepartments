11611e25d0a28df1f0365ab5ac61eae311cdf734
Pavel <pavel.gurinov@gmail.com>
2020-10-16 22:21:10 +1100

    connect heroku database

--- vendor/connect.php
+++ vendor/connect.php
@@ -2,15 +2,15 @@
 
 session_start();
 
-$user = 'root';
-$password = 'root';
-$db = 'test';
-$host = 'localhost';
+$user = 'b5736767aa4429';
+$password = 'f7147b76';
+$db = 'heroku_dd1b5361b8d310d';
+$host = 'eu-cdbr-west-03.cleardb.net';
 
 $connect = mysqli_connect($host, $user, $password, $db);
 
 if(!$connect){
   die("Connection failed:" . mysqli_connect_error());
 }
-
+mysql://b5736767aa4429:f7147b76@eu-cdbr-west-03.cleardb.net/heroku_dd1b5361b8d310d?reconnect=true
  ?>
