8905466541e7efd4e8eb6f7e00cee8dfb0b3831d
Pavel <pavel.gurinov@gmail.com>
2020-10-17 21:18:14 +1100

    on errors

--- auth/connect.php
+++ auth/connect.php
@@ -14,9 +14,12 @@ mysqli_query($connect, "CREATE TABLE IF NOT EXISTS users (
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
   )  ENGINE=INNODB");
-  
+
 if(!$connect){
   die("Connection failed:" . mysqli_connect_error());
 }
-mysql://b5736767aa4429:f7147b76@eu-cdbr-west-03.cleardb.net/heroku_dd1b5361b8d310d?reconnect=true
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
  ?>
